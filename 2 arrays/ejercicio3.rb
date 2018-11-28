fe0d2430d8c8e74ab45406f6a81499ee4712d944
Sebastian Poblete <sebapobletec@gmail.com>
2018-11-28 20:43:33 -0300

    Ejercicio3

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -5,3 +5,45 @@
  # 4. Crear un método que incrementa todos los elementos en una unidad y devuelva un arreglo nuevo.
 
 a = [1,2,3,9,1,4,5,2,3,6,6]
+
+
+def pares(array)
+  array.each_with_index do |value, index|
+    array.delete_at(index) if value.even?
+  end
+end
+
+pares(a)
+puts a
+
+def suma(array)
+  total = 0
+  array.each_with_index do |value, index|
+    total += array [index]
+  end
+  puts total
+end
+
+suma(a)
+
+def promedio(array)
+  total = 0
+  array.each_with_index do |value, index|
+    total += array [index]
+  end
+  promedio = total/array.length
+  puts promedio
+end
+
+promedio(a)
+
+
+def sumar1(array)
+b = []
+  array.each_with_index do |value, index|
+  b [index] = value+1
+  end
+  puts b
+end
+
+sumar1(a)
