ff2f5432dd097dabf2d44e99ec7303554a520f04
Sebastian Poblete <sebapobletec@gmail.com>
2018-11-28 19:47:28 -0300

    Ejercicio1

--- 2 arrays/ejercicio1.rb
+++ 2 arrays/ejercicio1.rb
@@ -6,3 +6,17 @@
 # 5. Mostrar en pantalla todos los elementos que se encuentren en una posición par.
 
 arreglo = [1,2,3,9,1,4,5,2,3,6,6]
+
+puts arreglo [0]
+
+puts arreglo [-1]
+
+puts arreglo
+
+for i in 0..(arreglo.length-1)
+puts "#{arreglo[i]} index #{i}"
+end
+
+for i in 0..(arreglo.length)
+puts "#{arreglo[i]}" if i.even?
+end
