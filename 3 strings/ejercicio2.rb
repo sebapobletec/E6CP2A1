b61525698c2835c04d2cdb58cf31d77c77eb861c
Sebastian Poblete <sebapobletec@gmail.com>
2018-11-28 21:05:27 -0300

    Ejercicio2

--- 3 strings/ejercicio2.rb
+++ 3 strings/ejercicio2.rb
@@ -2,3 +2,26 @@
 # 1. Imprimir todos los elementos que excedan más de 5 caracteres.
 # 2. Crear un arreglo nuevo con todos los elementos en minúscula.
 # 3. Crear un método que devuelva un arreglo con la cantidad de caracteres que tiene cada nombre.
+
+
+nombres =["Vannesa", "Marcos", "Sebastian", "Rodolfo", "Juan", "Jose"]
+
+nombres.each_with_index do |value, index|
+  puts value if value.length>5
+end
+
+nombresmin = []
+
+nombres.each_with_index do |value, index|
+  nombresmin[index] = value.downcase
+end
+
+puts nombresmin
+
+
+newarray = []
+nombres.each_with_index do |value, index|
+  newarray[index] =value.length
+end
+
+puts newarray
